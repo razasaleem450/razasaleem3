0f154b6bee40e70febeb1fc85b674ea95d9b15d4
razasaleem <raza@xoho.tech>
2018-05-03 20:26:01 +0500

    add .env.travis

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,11 +13,11 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\User::class, function (Faker\Generator $faker) {
-    static $password;
+    static $password='raza1234';
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'name' => 'raza',
+        'email' => 'raza@xoho.tech',
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
