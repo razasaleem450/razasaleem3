7e4b1b1269a615e172e65cf3031f36ead4993b62
razasaleem <raza@xoho.tech>
2018-05-08 19:29:34 +0500

    add deploy.sh

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 ssh -p22 witchschool@viva.isrv.tech <<EOF
 cd public_html
-git clone git@github.com:razasaleem450/razasaleem3.git .
 composer install
-
+php artisan migrate --seed
 <<EOF
